807f425c3378bf4867d00515da36ffe5b3e0ca07
Philip Salzmann <philip.salzmann@uibk.ac.at>
2021-08-16 11:56:05 +0200

    Install git in build containers

--- docker/common/install-system.sh
+++ docker/common/install-system.sh
@@ -5,7 +5,7 @@ set -eu
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get full-upgrade -y
-apt-get install -y --no-install-recommends build-essential python3 "$@"
+apt-get install -y --no-install-recommends build-essential python3 git "$@"
 
 # Don't keep around apt cache in docker image
 apt-get clean

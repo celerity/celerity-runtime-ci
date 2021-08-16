a24a2e2518055a4627448a3d8fdefab8019601f3
Philip Salzmann <philip.salzmann@uibk.ac.at>
2021-08-16 15:17:11 +0200

    Make sure user:user also exits in SYCL build containers

--- docker/common/setup-user.sh
+++ docker/common/setup-user.sh
@@ -3,12 +3,15 @@
 set -eu
 
 usage() {
-    echo "Usage: $0 <UID>" >&2
+    echo "Usage: $0 [<UID>]" >&2
     exit 1
 }
 
-if [ $# -ne 1 ]; then usage; fi
+if [ $# -gt 1 ]; then usage; fi
 
-UID="$1"
-
-useradd --create-home --uid "$UID" user
+if [ $# -eq 1 ]; then
+    UID="$1"
+    useradd --create-home --uid "$UID" user
+else
+    useradd --create-home user
+fi

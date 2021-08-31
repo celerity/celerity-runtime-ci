7528d92bd4805a73cb8610305b07d114fc95c4f0
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2021-08-31 17:01:20 +0200

    Work around OpenMPI 4.0.3 bug, require HDF5

--- computecpp/install/build-celerity.sh
+++ computecpp/install/build-celerity.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 set -eu
+
+# Workaround for https://bugs.launchpad.net/ubuntu/+source/openmpi/+bug/1941786
+export LDFLAGS=-lopen-pal
+
 sh /root/build-with-cmake.sh "$@" -- \
     -DCELERITY_SYCL_IMPL=ComputeCpp \
     -DComputeCpp_DIR="/opt/computecpp" \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCELERITY_EXAMPLES_REQUIRE_HDF5=ON
 
--- hipsycl/install/build-celerity.sh
+++ hipsycl/install/build-celerity.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 set -eu
+
+# Workaround for https://bugs.launchpad.net/ubuntu/+source/openmpi/+bug/1941786
+export LDFLAGS=-lopen-pal
+
 sh /root/build-with-cmake.sh "$@" -- \
     -DCELERITY_SYCL_IMPL=hipSYCL \
     -DCMAKE_PREFIX_PATH="/opt/hipsycl/lib/cmake" \
-    -DHIPSYCL_TARGETS=cuda:sm_75
+    -DHIPSYCL_TARGETS=cuda:sm_75 \
+    -DCELERITY_EXAMPLES_REQUIRE_HDF5=ON
 
     # TODO ccache is currently broken for hipSYCL because it uses compiler launchers internally
     # -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \

bb2142aeb0a3f8cfb8b64798f213c14e23fbb02d
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-01-05 13:33:39 +0100

    Enable ccache depend_mode for DPC++ to work around cache invalidation bug

--- dpcpp/install/build-options.sh
+++ dpcpp/install/build-options.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# We use Ninja instead of GNU Make to generate dependency files that are needed by ccache
+# in depend_mode. See Dockerfile and https://github.com/intel/llvm/issues/5260.
+
 CELERITY_BUILD_OPTS=(
-    -DCMAKE_C_COMPILER=/opt/dpcpp/bin/clang \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER=/opt/dpcpp/bin/clang++ \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-    -DCELERITY_SYCL_IMPL=DPC++ \
+    -DCMAKE_C_COMPILER=/opt/dpcpp/bin/clang
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_CXX_COMPILER=/opt/dpcpp/bin/clang++
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    -DCELERITY_SYCL_IMPL=DPC++
     -DCMAKE_INSTALL_PREFIX=/root/celerity-install
+    -G Ninja
 )
 
 EXAMPLES_BUILD_OPTS=(
-    -DCMAKE_PREFIX_PATH=/root/celerity-install/lib/cmake \
-    -DCMAKE_C_COMPILER=/opt/dpcpp/bin/clang \
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
-    -DCMAKE_CXX_COMPILER=/opt/dpcpp/bin/clang++ \
+    -DCMAKE_PREFIX_PATH=/root/celerity-install/lib/cmake
+    -DCMAKE_C_COMPILER=/opt/dpcpp/bin/clang
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_CXX_COMPILER=/opt/dpcpp/bin/clang++
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    -G Ninja
 )
 

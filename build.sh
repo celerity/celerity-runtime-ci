ec0448e574513ad594fefdd221ff2dc41f6f4360
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2022-06-09 16:41:43 +0200

    Vary Ubuntu base image version

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -eu -o pipefail
 
 usage() {
-    echo "Usage: $0 [-f|--force] hipsycl|dpcpp[:]<git-ref>" >&2
-    echo "       $0 [-f|--force] computecpp[:]<version>" >&2
+    echo "Usage: $0 [-f|--force] <ubuntu-version> hipsycl|dpcpp <git-ref>" >&2
+    echo "       $0 [-f|--force] <ubuntu-version> computecpp <version>" >&2
     exit 1
 }
 
@@ -20,12 +20,23 @@ while true; do
     esac
 done
 
-if [ $# -eq 1 ]; then set -- ${1//:/ }; fi  # split args on :
-if [ $# -ne 2 ]; then usage; fi
-LIBRARY="$1"
-REF="$2"
+if [ $# -ne 3 ]; then usage; fi
+UBUNTU="$1"
+SYCL="$2"
+REF="$3"
 
-LIB_DIR="$(readlink -f "$(dirname "$0")")/$LIBRARY"
+unset CUDA
+if [ "$SYCL" == hipsycl ]; then
+	if [ "$UBUNTU" == 20.04 ]; then
+		CUDA=11.0.3
+	elif [ "$UBUNTU" == 22.04 ]; then
+		CUDA=11.7.0
+	else
+		echo "I don't know which CUDA version to select for Ubuntu $UBUNTU"
+	fi 
+fi
+
+LIB_DIR="$(readlink -f "$(dirname "$0")")/$SYCL"
 cd "$LIB_DIR"
 
 build-from-source() {
@@ -36,7 +47,7 @@ build-from-source() {
 
     SRC_BASE_DIR="$HOME/celerity-src"
     mkdir -p "$SRC_BASE_DIR"
-    SRC_DIR="$SRC_BASE_DIR/$LIBRARY"
+    SRC_DIR="$SRC_BASE_DIR/$SYCL"
     if [ -e "$SRC_DIR" ]; then
         cd "$SRC_DIR"
         git fetch --all
@@ -53,15 +64,16 @@ build-from-source() {
     git -c advice.detachedHead=false checkout --force "$COMMIT_ID"
     git clean -fdx
     git submodule update --init --recursive
-    VERSION="$(git log --format=format:"$LIBRARY $REF @ %H | %ci" -1)"
+    VERSION="Ubuntu ${UBUNTU} ${CUDA+CUDA: $CUDA }$SYCL $REF @ $(git log --format=format:"%H | %ci" -1)"
 
     cd "$LIB_DIR"
     echo "Building $VERSION"
 
-    BUILD_IMAGE_NAME="build/$LIBRARY"
-    IMAGE_NAME="build/celerity/$LIBRARY"
-    COMMIT_TAG="$IMAGE_NAME:$COMMIT_ID"
-    GIT_REF_TAG="$IMAGE_NAME:$(echo -n "$REF" | tr -sc 'A-Za-z0-9.' '-')"
+    BUILD_IMAGE_TAG="build/$SYCL:ubuntu$UBUNTU"
+    IMAGE_NAME="build/celerity/$SYCL"
+	BASE_TAG="$IMAGE_NAME:ubuntu$UBUNTU"
+    COMMIT_TAG="$BASE_TAG-$COMMIT_ID"
+    GIT_REF_TAG="$BASE_TAG-$(echo -n "$REF" | tr -sc 'A-Za-z0-9.' '-')"
 
     EXISTING_IMAGE_ID="$(docker images -f "reference=$COMMIT_TAG" -q)"
     if [ -n "$EXISTING_IMAGE_ID" ] && ! [ -n "${FORCE+x}" ]; then
@@ -72,15 +84,17 @@ build-from-source() {
 
     # We use a named volume for ccache.
     # The volume is managed by Docker and persists between containers.
-    CCACHE_VOLUME="ccache-$LIBRARY"
+    CCACHE_VOLUME="ccache-$SYCL"
 
-    echo "Creating SYCL build container ($BUILD_IMAGE_NAME:latest)"
+    echo "Creating SYCL build container ($BUILD_IMAGE_TAG)"
     cp -r ../common build
-    docker build build --tag "$BUILD_IMAGE_NAME:latest"
+    docker build build \
+		--build-arg=UBUNTU="$UBUNTU" ${CUDA+"--build-arg=CUDA=$CUDA"} \
+		--tag "$BUILD_IMAGE_TAG"
     BUILD_IMAGE_CONTAINER_ID=$(docker create \
         --mount "type=bind,src=$SRC_DIR,dst=/src" \
         --mount "type=volume,src=$CCACHE_VOLUME,dst=/ccache" \
-        "$BUILD_IMAGE_NAME:latest"
+        "$BUILD_IMAGE_TAG"
     )
 
     function cleanup {
@@ -92,12 +106,14 @@ build-from-source() {
     echo "Starting SYCL build container ($BUILD_IMAGE_CONTAINER_ID)"
     docker start --attach "$BUILD_IMAGE_CONTAINER_ID"
     echo "Copying installation files to host"
-    docker cp "$BUILD_IMAGE_CONTAINER_ID:/opt/$LIBRARY" "$PWD/install/opt"
+    docker cp "$BUILD_IMAGE_CONTAINER_ID:/opt/$SYCL" "$PWD/install/opt"
 
     echo "Building Celerity build container"
     cp -r ../common install
     echo "$VERSION" > install/VERSION
-    docker build --tag "$COMMIT_TAG" --tag "$GIT_REF_TAG" install
+    docker build \
+		--build-arg=UBUNTU="$UBUNTU" ${CUDA+"--build-arg=CUDA=$CUDA"} \
+		--tag "$COMMIT_TAG" --tag "$GIT_REF_TAG" install
 }
 
 build-from-distribution() {
@@ -106,12 +122,13 @@ build-from-distribution() {
     # log to a file to avoid choking tar on head -1 and triggering set -e
     tar tf "$TARBALL" > /tmp/tarlist
     PACKAGE_NAME="$(head -1 /tmp/tarlist | cut -d/ -f1)"
-    VERSION="$LIBRARY $REF"
+    VERSION="Ubuntu ${UBUNTU} ${CUDA+CUDA: $CUDA }$SYCL $REF"
 
     echo "Building $VERSION"
 
-    IMAGE_NAME="build/celerity/$LIBRARY"
-    SYMBOLIC_TAG="$IMAGE_NAME:$(echo -n "$REF" | tr -sc 'A-Za-z0-9.' '-')"
+    IMAGE_NAME="build/celerity/$SYCL"
+	BASE_TAG="$IMAGE_NAME:ubuntu$UBUNTU"
+    SYMBOLIC_TAG="$BASE_TAG-$(echo -n "$REF" | tr -sc 'A-Za-z0-9.' '-')"
 
     EXISTING_IMAGE_ID="$(docker images -f "reference=$SYMBOLIC_TAG" -q)"
     if [ -n "$EXISTING_IMAGE_ID" ] && ! [ -n "${FORCE+x}" ]; then
@@ -123,16 +140,18 @@ build-from-distribution() {
     mkdir -p install/opt
     cd install/opt
     tar xf "$TARBALL"
-    mv "$PACKAGE_NAME" "$LIBRARY"
+    mv "$PACKAGE_NAME" "$SYCL"
     cd ../..
 
     cp -r ../common install
     echo "$VERSION" > install/VERSION
-    docker build --tag "$SYMBOLIC_TAG" install
+    docker build \
+		--build-arg=UBUNTU="$UBUNTU" ${CUDA+"--build-arg=CUDA=$CUDA"} \
+		--tag "$SYMBOLIC_TAG" install
 }
 
-case "$LIBRARY" in
-    hipsycl) build-from-source "https://github.com/illuhad/hipSYCL.git";;
+case "$SYCL" in
+	hipsycl) build-from-source "https://github.com/illuhad/hipSYCL.git";;
     dpcpp) build-from-source "https://github.com/intel/llvm.git";;
     computecpp)
         set -- ${REF//-/ }  # split args on -
@@ -141,6 +160,7 @@ case "$LIBRARY" in
         SYSTEM=x86_64-linux-gnu
         build-from-distribution "$(pwd)/dist/$DISTRIBUTION-$VERSION-$SYSTEM.tar"*
         ;;
+
     *) usage;;
 esac
 
--- common/install-system.sh
+++ common/install-system.sh
@@ -6,26 +6,44 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get full-upgrade -y
 
+PACKAGES=( "$@" )
+
+source /etc/lsb-release
+
+# Some package names depend on the LLVM version (=> Ubuntu version)
+case "$DISTRIB_RELEASE" in
+	20.04) LLVM=10;;
+	22.04) LLVM=14;;
+	*) echo "Unsupported Ubuntu version $DISTRIB_RELEASE">&2; exit 1;;
+esac
+
+# Substitute our made-up package names with Ubuntu versioned names
+for i in "${!PACKAGES[@]}"; do
+	case "${PACKAGES[i]}" in
+		libomp-dev) PACKAGES[i]="libomp-$LLVM-dev";;
+	esac
+done
+
 if [ $# -gt 0 ]; then
-    apt-get install -y --no-install-recommends "$@"
+    apt-get install -y --no-install-recommends "${PACKAGES[@]}"
 fi
 
 # Don't keep around apt cache in docker image
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
-if [[ " $@ " =~ " libhdf5-openmpi-dev " ]]; then
+if [[ " ${PACKAGES[*]} " =~ " libhdf5-openmpi-dev " ]]; then
     # HDF5 pkg-config exports an incorrect include path, which CMake chokes on
     ln -s /usr/lib/x86_64-linux-gnu/openmpi/include/openmpi /usr/include/openmpi
 fi
 
 # Use lld or GNU gold for faster linking
-if [[ " $@ " =~ " lld " ]]; then
+if [[ " ${PACKAGES[*]} " =~ " lld " ]]; then
     update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.lld" 30
 fi
 update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 20
 update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.bfd" 10
 
-if [[ " $@ " =~ " gdb " ]]; then
+if [[ " ${PACKAGES[*]} " =~ " gdb " ]]; then
     echo 'set auto-load safe-path /' >> /etc/gdb/gdbinit
 fi

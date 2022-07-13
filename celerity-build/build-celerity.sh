d0f5f1b96e20f9715bb6cc14f0620b381ca24366
Philip Salzmann <philip.salzmann@uibk.ac.at>
2022-07-13 10:17:58 +0200

    Set CMAKE_EXPORT_COMPILE_COMMANDS on Celerity builds
    We need compile commands for running clang-tidy.

--- celerity-build/build-celerity.sh
+++ celerity-build/build-celerity.sh
@@ -7,7 +7,9 @@ source /root/celerity-options.sh
 # Workaround for https://bugs.launchpad.net/ubuntu/+source/openmpi/+bug/1941786
 export LDFLAGS=-lopen-pal
 
+# Export compile commands for clang-tidy checks
 bash /root/build-with-cmake.sh /root/build "$@" -- \
     "${CELERITY_BUILD_OPTS[@]}" \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCELERITY_EXAMPLES_REQUIRE_HDF5=ON \
 

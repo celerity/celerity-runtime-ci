677031003e54c11e9d033363f58bdd76cbfe3c6a
Philip Salzmann <philip.salzmann@uibk.ac.at>
2023-03-27 11:42:35 +0200

    Bump CUDA versions for hipSYCL containers
    We need at least 11.3 because earlier containers for some reason don't
    include the "cuda-toolkit" package (which we now require in Celerity for
    the CUDA backend).
    
    Also bump the 22.04 container to use 12.1, the newest version available.

--- build.sh
+++ build.sh
@@ -39,8 +39,8 @@ unset INTEL_COMPUTE_RT
 unset INTEL_IGC
 if [ "$SYCL" == hipsycl ]; then
     case "$UBUNTU" in
-        20.04) CUDA=11.0.3;;
-        22.04) CUDA=11.7.0;;
+        20.04) CUDA=11.3.1;;
+        22.04) CUDA=12.1.0;;
         *) echo "I don't know which CUDA version to select for Ubuntu $UBUNTU" >&2; exit 1;;
     esac
 else

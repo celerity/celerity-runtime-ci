e15610f226c6e0807cea0b44f5ee7bc8ac6f7b06
Philip Salzmann <philip.salzmann@uibk.ac.at>
2023-07-14 15:22:33 +0200

    Bring back ccache for hipSYCL
    This should work with hipSYCL builds starting from June 2023
    (https://github.com/OpenSYCL/OpenSYCL/pull/1065).

--- hipsycl/ndzip-options.sh
+++ hipsycl/ndzip-options.sh
@@ -2,11 +2,10 @@
 
 NDZIP_BUILD_OPTS=(
     -DCMAKE_C_COMPILER="/usr/bin/clang"
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache
     -DCMAKE_CXX_COMPILER="/usr/bin/clang++"
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
     -DCMAKE_PREFIX_PATH="/opt/hipsycl/lib/cmake"
     -DHIPSYCL_TARGETS=cuda:sm_75
 )
 
-# TODO ccache is currently broken for hipSYCL because it uses compiler launchers internally
-#     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-#     -DCMAKE_C_COMPILER_LAUNCHER=ccache

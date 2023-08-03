81beacbc80833aab79a39c3e338f62f5feace5ab
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2023-08-03 15:37:36 +0200

    Temporarily disable ccache for hipSYCL

--- hipsycl/celerity-options.sh
+++ hipsycl/celerity-options.sh
@@ -2,9 +2,7 @@
 
 CELERITY_BUILD_OPTS=(
     -DCMAKE_C_COMPILER=/usr/bin/clang
-    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCELERITY_SYCL_IMPL=hipSYCL 
     -DCMAKE_PREFIX_PATH=/opt/hipsycl
     -DHIPSYCL_TARGETS=cuda:sm_75
@@ -13,9 +11,7 @@ CELERITY_BUILD_OPTS=(
 
 EXAMPLES_BUILD_OPTS=(
     -DCMAKE_C_COMPILER=/usr/bin/clang
-    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCMAKE_PREFIX_PATH="/opt/hipsycl;/root/celerity-install"
     -DHIPSYCL_TARGETS=cuda:sm_75
 )

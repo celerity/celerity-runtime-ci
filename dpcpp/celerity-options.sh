4233731cd8ed94a438876c80ddd297d5230c18b1
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2023-08-03 14:14:39 +0200

    Update ccache paths and environment variables

--- dpcpp/celerity-options.sh
+++ dpcpp/celerity-options.sh
@@ -5,9 +5,9 @@
 
 CELERITY_BUILD_OPTS=(
     -DCMAKE_C_COMPILER=/opt/dpcpp/bin/clang
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCMAKE_CXX_COMPILER=/opt/dpcpp/bin/clang++
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCELERITY_SYCL_IMPL=DPC++
     -DCMAKE_INSTALL_PREFIX=/root/celerity-install
     -G Ninja
@@ -16,9 +16,9 @@ CELERITY_BUILD_OPTS=(
 EXAMPLES_BUILD_OPTS=(
     -DCMAKE_PREFIX_PATH=/root/celerity-install/lib/cmake
     -DCMAKE_C_COMPILER=/opt/dpcpp/bin/clang
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache
+    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCMAKE_CXX_COMPILER=/opt/dpcpp/bin/clang++
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
+    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache
     -G Ninja
 )
 
--- hipsycl/celerity-options.sh
+++ hipsycl/celerity-options.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 CELERITY_BUILD_OPTS=(
-    -DCMAKE_C_COMPILER="/usr/bin/clang"
-    -DCMAKE_CXX_COMPILER="/usr/bin/clang++"
+    -DCMAKE_C_COMPILER=/usr/bin/clang
+    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache
+    -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache
     -DCELERITY_SYCL_IMPL=hipSYCL 
-    -DCMAKE_PREFIX_PATH="/opt/hipsycl/lib/cmake"
+    -DCMAKE_PREFIX_PATH=/opt/hipsycl
     -DHIPSYCL_TARGETS=cuda:sm_75
     -DCMAKE_INSTALL_PREFIX=/root/celerity-install
 )
 
 EXAMPLES_BUILD_OPTS=(
-    -DCMAKE_C_COMPILER="/usr/bin/clang"
-    -DCMAKE_C_COMPILER_LAUNCHER=ccache
-    -DCMAKE_CXX_COMPILER="/usr/bin/clang++"
-    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-    -DCMAKE_PREFIX_PATH="/opt/hipsycl/lib/cmake;/root/celerity-install/lib/cmake"
+    -DCMAKE_C_COMPILER=/usr/bin/clang
+    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache
+    -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache
+    -DCMAKE_PREFIX_PATH="/opt/hipsycl;/root/celerity-install"
     -DHIPSYCL_TARGETS=cuda:sm_75
 )
 

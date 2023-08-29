4ae5995732cde46ad7af3bd011fca684a512bb92
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2023-08-29 11:23:19 +0200

    Disable all except CUDA backends for hipSYCL

--- hipsycl/build/build-hipsycl.sh
+++ hipsycl/build/build-hipsycl.sh
@@ -5,6 +5,8 @@ rm -rf /opt/hipsycl/*
 bash ~/build-with-cmake.sh /root/build /src --target install -- \
     -DCMAKE_PREFIX_PATH=/usr/lib/llvm-10/lib/cmake/llvm \
     -DWITH_CUDA_BACKEND=YES \
+    -DWITH_ROCM_BACKEND=NO \
+    -DWITH_OPENCL_BACKEND=NO \
     -DCMAKE_INSTALL_PREFIX=/opt/hipsycl \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache

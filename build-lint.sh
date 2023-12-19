c9ec415c844453f7751ff70ceb91a2e73309f243
Philip Salzmann <philip.salzmann@uibk.ac.at>
2023-12-19 14:45:30 +0100

    Base linting container on celerity-build container
    Specifically on celerity-build/hipsycl:ubuntu22.04-d2bd9fc7.
    
    This way we can run clang-tidy inside this container (which
    requires a compile_commands.json).

--- build-lint.sh
+++ build-lint.sh
@@ -2,8 +2,23 @@
 
 set -eu -o pipefail
 
+usage() {
+    echo "Usage: $0 <ubuntu-version> hipsycl|dpcpp <git-ref>" >&2
+    echo "Builds the linting container on top of an existing celerity-build container." >&2
+    exit 1
+}
+
+if [ $# -ne 3 ]; then usage; fi
+UBUNTU="$1"
+SYCL="$2"
+SYCL_REF="$3"
+
 WORKING_DIR="$(readlink -f "$(dirname "$0")")/lint"
 cd "$WORKING_DIR"
 cp -r ../common .
 
-docker build . --tag "celerity-lint:latest"
+docker build . \
+    --build-arg=UBUNTU="${UBUNTU}" \
+    --build-arg=SYCL="${SYCL}" \
+    --build-arg=SYCL_REF="${SYCL_REF}" \
+    --tag "celerity-lint:latest"
